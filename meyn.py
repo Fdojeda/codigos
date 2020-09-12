057df353ec4ed9bc1209737ddaa9cba8a34e7792
pancito13 <ccisnerosz@uc.cl>
2020-09-11 21:28:52 -0300

    lmeo

---
+++ meyn.py
@@ -0,0 +1,17 @@
+
+class Menu:
+    def __init__(self):
+        self.atributo = None
+
+    def menu_entrenador(self):
+        return "Este es el menu de entrenador"
+
+    def simular_competencias(self):
+        return "Esto es para simular competencias"
+
+    def mostrar_estado(self):
+        return "Esto es para mostrar Estado"
+
+    def salir_programa(self):
+        return "saliste"
+
